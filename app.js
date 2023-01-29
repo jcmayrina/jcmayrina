3c912dfc36ab475205a6cfb1466e6b66654e4708
James Carlo C. Mayrina <jamescarlomayrina@gmail.com>
2023-01-29 20:47:22 +0800

    diff js for mobile and pc

--- app.js
+++ app.js
@@ -1,57 +1,114 @@
-gsap.to(".my-name1", {
-  scrollTrigger: {
-    trigger: ".my-name1",
-    scrub: 3,
-    pin: true,
-    start: "top top",
-    end: "+=120%",
-    pinSpacing: false,
-  },
-  color: "black",
-  immediateRender: false,
-  x: 50,
-  y: 300,
-});
-gsap.to(".my-name2", {
-  scrollTrigger: {
-    trigger: ".my-name2",
-    scrub: 3,
-    pin: true,
-    start: "top top",
-    end: "+=120%",
-    pinSpacing: false,
-  },
-  color: "black",
-  immediateRender: false,
-  x: -50,
-  y: 300,
-});
+var w = window.innerWidth;
+if (w <= 650) {
+  gsap.to(".my-name1", {
+    scrollTrigger: {
+      trigger: ".my-name1",
+      scrub: 3,
+      pin: true,
+      start: "top top",
+      end: "+=120%",
+      pinSpacing: false,
+    },
+    color: "black",
+    immediateRender: false,
+    x: 10,
+    y: 420,
+  });
+  gsap.to(".my-name2", {
+    scrollTrigger: {
+      trigger: ".my-name2",
+      scrub: 3,
+      pin: true,
+      start: "top top",
+      end: "+=120%",
+      pinSpacing: false,
+    },
+    color: "black",
+    immediateRender: false,
+    x: -10,
+    y: 420,
+  });
 
-gsap.to(".my-name3", {
-  scrollTrigger: {
-    trigger: ".my-name3-con",
-    scrub: 4,
-    pin: true,
-    start: "top top",
-    end: "bottom",
-    pinSpacing: false,
-  },
-  y: 1100,
-  color: "white",
-  immediateRender: false,
-});
+  gsap.to(".my-name3", {
+    scrollTrigger: {
+      trigger: ".my-name3-con",
+      scrub: 10,
+      pin: true,
+      start: "-250px",
+      end: "bottom",
+      pinSpacing: false,
+      markers: true,
+    },
+    x: 0,
+    y: 900,
+    color: "white",
+    immediateRender: false,
+  });
 
-gsap.to(".my-proj", {
-  scrollTrigger: {
-    trigger: ".my-img-end",
-    scrub: 0.1,
-    start: "top top",
-    bottom: "bottom 50%",
-  },
-});
-ScrollTrigger.create({
-  snap: {
-    snapTo: 1 / 2,
-    duration: 0.5,
-  },
-});
+  gsap.to(".my-proj", {
+    scrollTrigger: {
+      trigger: ".my-img-end",
+      scrub: 0.1,
+      start: "top top",
+      bottom: "bottom 50%",
+    },
+  });
+} else {
+  gsap.to(".my-name1", {
+    scrollTrigger: {
+      trigger: ".my-name1",
+      scrub: 3,
+      pin: true,
+      start: "top top",
+      end: "+=120%",
+      pinSpacing: false,
+    },
+    color: "black",
+    immediateRender: false,
+    x: 10,
+    y: 320,
+  });
+  gsap.to(".my-name2", {
+    scrollTrigger: {
+      trigger: ".my-name2",
+      scrub: 3,
+      pin: true,
+      start: "top top",
+      end: "+=120%",
+      pinSpacing: false,
+    },
+    color: "black",
+    immediateRender: false,
+    x: -10,
+    y: 320,
+  });
+
+  gsap.to(".my-name3", {
+    scrollTrigger: {
+      trigger: ".my-name3-con",
+      scrub: 4,
+      pin: true,
+      start: "top top",
+      end: "bottom",
+      pinSpacing: false,
+    },
+    y: 1100,
+    color: "white",
+    immediateRender: false,
+  });
+
+  gsap.to(".my-proj", {
+    scrollTrigger: {
+      trigger: ".my-img-end",
+      scrub: 0.1,
+      start: "top top",
+      bottom: "bottom 50%",
+    },
+  });
+  ScrollTrigger.create({
+    snap: {
+      snapTo: 1 / 4,
+      duration: 0.5,
+    },
+  });
+}
