acc90641b262b357d20473ad3a9e0364b8a2a8a4
James Carlo C. Mayrina <jamescarlomayrina@gmail.com>
2023-02-06 00:16:08 +0800

    many changes (changed the gsap  for desktop)

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ if (w <= 650) {
       end: "+=120%",
       pinSpacing: false,
     },
-    color: "black",
+    color: "white",
     immediateRender: false,
     x: 10,
     y: 420,
@@ -23,7 +23,7 @@ if (w <= 650) {
       end: "+=120%",
       pinSpacing: false,
     },
-    color: "black",
+    color: "white",
     immediateRender: false,
     x: -10,
     y: 420,
@@ -56,58 +56,93 @@ if (w <= 650) {
   gsap.to(".my-name1", {
     scrollTrigger: {
       trigger: ".my-name1",
+      start: "-150",
+      end: "bottom",
       scrub: 3,
-      pin: true,
-      start: "top top",
-      end: "+=120%",
-      pinSpacing: false,
     },
-    color: "black",
-    immediateRender: false,
-    x: 10,
-    y: 320,
+    opacity: 0,
   });
   gsap.to(".my-name2", {
     scrollTrigger: {
-      trigger: ".my-name2",
+      trigger: ".my-name1",
+      start: "-150",
+      end: "bottom",
       scrub: 3,
-      pin: true,
-      start: "top top",
-      end: "+=120%",
-      pinSpacing: false,
     },
-    color: "black",
-    immediateRender: false,
-    x: -10,
-    y: 320,
+    opacity: 0,
   });
 
   gsap.to(".my-name3", {
     scrollTrigger: {
       trigger: ".my-name3-con",
-      scrub: 4,
-      pin: true,
-      start: "top top",
+      start: "-100",
       end: "bottom",
-      pinSpacing: false,
+      scrub: 3,
+      pin: true,
     },
-    y: 1200,
-    color: "white",
-    immediateRender: false,
+    opacity: 0,
   });
 
-  gsap.to(".my-proj", {
+  gsap.to(".scroll-down", {
     scrollTrigger: {
-      trigger: ".my-img-end",
-      scrub: 0.1,
-      start: "top top",
-      bottom: "bottom 50%",
+      trigger: ".my-name3",
+      start: "400",
+      end: "650",
+      scrub: 3,
     },
+    opacity: 0,
   });
-  ScrollTrigger.create({
-    snap: {
-      snapTo: 1 / 4,
-      duration: 0.5,
+
+  gsap.to(".scroll-text", {
+    scrollTrigger: {
+      trigger: ".my-name3",
+      start: "300",
+      end: "600",
+      scrub: 3,
     },
+    opacity: 0,
+  });
+
+  gsap.to(".my-job-name", {
+    scrollTrigger: {
+      trigger: ".my-name3",
+      start: "top top",
+      end: "bottom",
+      scrub: 3,
+    },
+    opacity: 0,
+  });
+
+  gsap.to(".my-name1b", {
+    scrollTrigger: {
+      trigger: ".my-name1b",
+      start: "-550",
+      end: "-450",
+      scrub: 6,
+      toggleActions: "play none none reverse",
+    },
+    x: 200,
+    opacity: 1,
+  });
+  gsap.to(".my-name2b", {
+    scrollTrigger: {
+      trigger: ".my-name1b",
+      start: "-550",
+      end: "-450",
+      scrub: 6,
+      toggleActions: "play none none reverse",
+    },
+    x: -200,
+    opacity: 1,
+  });
+
+  gsap.to(".my-name3b", {
+    scrollTrigger: {
+      trigger: ".my-name3b-con",
+      start: "-400",
+      end: "-450",
+      scrub: 4,
+    },
+    y: -50,
   });
 }
