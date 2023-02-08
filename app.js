ba6be3a00d4eb90eb6e1d5239280228aa6e5a971
James Carlo C. Mayrina <jamescarlomayrina@gmail.com>
2023-02-08 13:35:27 +0800

    desktop done!

--- app.js
+++ app.js
@@ -107,3 +107,40 @@ cards.forEach((card) => {
     ease: "expo.inOut",
   });
 });
+function link1() {
+  window.open("https://github.com/jcmayrina/suntrust.github.io", "_blank");
+}
+function link2() {
+  window.open("https://github.com/jcmayrina/RestaurantProject", "_blank");
+}
+function link3() {
+  window.open("https://github.com/jcmayrina/lakbai", "_blank");
+}
+//document.body.style.cursor = "none";
+let innerC = document.querySelector(".inner-cursor");
+let outerC = document.querySelector(".outer-cursor");
+
+document.addEventListener("mousemove", moveCursor);
+
+function moveCursor(e) {
+  let x = e.clientX;
+  let y = e.clientY;
+  innerC.style.left = `${x}px`;
+  innerC.style.top = `${y}px`;
+  outerC.style.left = `${x}px`;
+  outerC.style.top = `${y}px`;
+}
+let links = Array.from(document.querySelectorAll(".linkhover"));
+links.forEach((link) => {
+  link.addEventListener("mouseover", () => {
+    innerC.classList.add("grow");
+  });
+  link.addEventListener("mouseleave", () => {
+    innerC.classList.remove("grow");
+  });
+});
+
+function scrollProjects() {
+  var proj = document.getElementById("projects");
+  proj.scrollIntoView({ behavior: "smooth" });
+}
